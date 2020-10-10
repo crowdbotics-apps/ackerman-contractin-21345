7bfc04a70d3c853b6b837bae8ad087ef7ef1fe00
Crowdbotics <team@crowdbotics.com>
2020-10-10 21:01:08 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/ackerman_contractin_21345/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ackerman_contractin_21345;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "ackerman_contractin_21345";
+    }
+}
